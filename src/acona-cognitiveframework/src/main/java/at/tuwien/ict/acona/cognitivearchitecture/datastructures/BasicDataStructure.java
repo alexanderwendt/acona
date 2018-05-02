36527f622ca7646af2895a70dba7a79e1e359461
wendt <alexander.wendt@tuwien.ac.at>
2018-05-02 17:40:19 +0200

    Minor changes

--- src/acona-cognitiveframework/src/main/java/at/tuwien/ict/acona/cognitivearchitecture/datastructures/BasicDataStructure.java
+++ src/acona-cognitiveframework/src/main/java/at/tuwien/ict/acona/cognitivearchitecture/datastructures/BasicDataStructure.java
@@ -4,48 +4,50 @@ import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
 public abstract class BasicDataStructure {
-	//protected transient DecimalFormat df = new DecimalFormat("#.##");
-	
+	// protected transient DecimalFormat df = new DecimalFormat("#.##");
+
 	private String name;
 	private String type;
-	//private String id;
-	
+	// private String id;
+
 	public BasicDataStructure(String name, String type) {
 		super();
 		this.name = name;
 		this.type = type;
-		
-		//this.id = this.name + this.as
+
+		// this.id = this.name + this.as
 	}
-	
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getType() {
 		return type;
 	}
-	
+
 	public boolean isNull() {
 		boolean result = false;
-		if (this.name==null) {
-			result=true;
+		if (this.name == null || this.name.isEmpty()) {
+			result = true;
 		}
-		
+
 		return result;
 	}
-	
+
 	public JsonObject toJsonObject() {
 		Gson gson = new Gson();
-		
+
 		return gson.fromJson(gson.toJson(this), JsonObject.class);
 	}
 //	public void setType(String type) {
 //		this.type = type;
 //	}
-	
+
 //	public String getId() {
 //		return id;
 //	}
